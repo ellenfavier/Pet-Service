c95cf61820bf175024ec626d196ecf449c46d0b8
ellenfavier <62351301+ellenfavier@users.noreply.github.com>
2025-11-13 23:53:53 +0500

    Delete pet-service directory

--- pet-service/app/domain/entities.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class PetState(BaseModel):
-    id: Optional[str] = None
-    user_id: str
-    mood: int
-    health: int
--- pet-service/app/infrastructure/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-from app.settings import settings
-
-# Используем 'settings' вместо 'os.getenv'
-client = MongoClient(settings.MONGO_URI)
-db = client[settings.DB_NAME]
